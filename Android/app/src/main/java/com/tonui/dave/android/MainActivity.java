3810ca91317fb037009711c7463c47d6e89070f7
tonuidavies <tonuidavies@gmail.com>
2016-05-30 17:36:38 +0300

    a 30seconds splash screen

---
+++ Android/app/src/main/java/com/tonui/dave/android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tonui.dave.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
